c456e581f317f1d4e8b42aede4aff293d408da0e
Markus Himmel <markus@himmel-villmar.de>
2021-10-24 00:55:25 +0200

    Sorry free

--- src/cnf.lean
+++ src/cnf.lean
@@ -20,13 +20,36 @@ end
 
 namespace literal
 
+@[simp]
 def inverse : literal α → literal α
 | (pos x) := neg x
 | (neg x) := pos x
 
+@[simp]
+def var : literal α → α
+| (pos x) := x
+| (neg x) := x
+
+@[simp]
 def satisfied (ι : interpretation α) : literal α → bool
 | (pos x) := ι x
 | (neg x) := bnot (ι x)
+
+@[simp]
+lemma satisfied_inverse (ι : interpretation α) (l : literal α) : satisfied ι l.inverse = bnot (satisfied ι l) :=
+by cases l; simp
+
+@[simp]
+lemma inverse_inverse (l : literal α) : l.inverse.inverse = l :=
+by cases l; refl
+
+@[simp]
+lemma bnot_iff_not (b : bool) : !b ↔ ¬b :=
+by { unfold_coes, simp only [bnot_eq_true_eq_eq_ff, eq_ff_eq_not_eq_tt] }
+
+lemma not_satisfied_and_satisfied_inverse (ι : interpretation α) (l m : literal α)
+  (hl : satisfied ι l) (hm : satisfied ι m) : l ≠ m.inverse :=
+λ h, absurd hl $ by { convert hm, simp [h] }
 
 end literal
 
@@ -35,9 +58,24 @@ namespace clause
 def satisfied (ι : interpretation α) (c : clause α) : bool :=
 c.any (literal.satisfied ι)
 
+lemma satisfied_eq {ι κ : interpretation α} {c : clause α} (h : ∀ l ∈ c, literal.satisfied ι l = literal.satisfied κ l) :
+  c.satisfied ι = c.satisfied κ :=
+begin
+  induction c with l ls ih,
+  { refl },
+  { rw [satisfied, satisfied, list.any_cons, list.any_cons, h l (list.mem_cons_self _ _), ←satisfied, ←satisfied,
+      ih (λ m hm, h _ (list.mem_cons_of_mem _ hm))] }
+end
+
+lemma satisfied_iff (ι : interpretation α) (c : clause α) : satisfied ι c ↔ ∃ l ∈ c, literal.satisfied ι l :=
+by rw [satisfied, list.any_iff_exists]
+
 @[simp] 
 lemma not_satisfied_empty (ι : interpretation α) : satisfied ι [] = ff :=
 rfl
+
+lemma not_satisfied_empty' (ι : interpretation α) : ¬satisfied ι [] :=
+by simp
 
 end clause
 
@@ -46,10 +84,43 @@ namespace cnf
 def satisfied (ι : interpretation α) (c : cnf α) : bool :=
 c.all (clause.satisfied ι)
 
+lemma satisfied_eq (ι κ : interpretation α) {c : cnf α} (h : ∀ γ ∈ c, ∀ l ∈ γ, literal.satisfied ι l = literal.satisfied κ l) :
+  c.satisfied ι = c.satisfied κ :=
+begin
+  induction c with γ c ih,
+  { refl },
+  { rw [satisfied, satisfied, list.all_cons, list.all_cons,
+      clause.satisfied_eq (λ l hl, h _ (list.mem_cons_self _ _) _ hl), ←satisfied, ←satisfied,
+      ih (λ δ hδ l hl, h _ (list.mem_cons_of_mem _ hδ) _ hl)] }
+end
+
+lemma satisfied_iff (ι : interpretation α) (c : cnf α) : satisfied ι c ↔ ∀ γ ∈ c, clause.satisfied ι γ :=
+by rw [satisfied, list.all_iff_forall]
+
 def satisfiable (c : cnf α) : Prop :=
 ∃ ι, satisfied ι c
 
+@[simp]
 lemma satisfiable_empty : satisfiable ([] : cnf α) :=
 ⟨λ x, false, dec_trivial⟩
+
+lemma not_satisfiable_of_empty_mem {c : cnf α} (hc : [] ∈ c) : ¬satisfiable c :=
+begin
+  rw satisfiable,
+  rintro ⟨ι, hι⟩,
+  rw satisfied_iff at hι,
+  exact clause.not_satisfied_empty' ι (hι _ hc)
+end
+
+#print axioms not_satisfiable_of_empty_mem
+
+def size (c : cnf α) : ℕ :=
+(c.map list.length).sum
+
+instance : has_sizeof (cnf α) :=
+{ sizeof := size }
+
+lemma sizeof_eq_size (c : cnf α) : sizeof c = size c :=
+rfl
 
 end cnf
--- src/dpll.lean
+++ src/dpll.lean
@@ -4,6 +4,57 @@ universe u
 
 variables {α : Type u} [decidable_eq α]
 
+namespace list
+
+lemma count_lt_iff_exists (l : list α) (P : α → Prop) [decidable_pred P] : (l.filter P).length < l.length ↔ ∃ x ∈ l, ¬P x :=
+begin
+  refine ⟨_, _⟩,
+  { induction l with a as ih,
+    { dec_trivial },
+    { by_cases h : P a,
+      { rw [list.length_cons, list.filter_cons_of_pos _ h, list.length_cons, add_lt_add_iff_right],
+        intro h',
+        obtain ⟨x, ⟨hx, hx'⟩⟩ := ih h',
+        exact ⟨x, ⟨mem_cons_of_mem a hx, hx'⟩⟩ },
+      { exact λ _, ⟨a, ⟨mem_cons_self _ _, h⟩⟩ } } },
+  { rintro ⟨x, ⟨hx, hx'⟩⟩,
+    obtain ⟨s, t, h⟩ := list.mem_split hx,
+    subst h,
+    simp only [filter_append, length, length_append, filter_cons_of_neg _ hx'],
+    calc (filter P s).length + (filter P t).length ≤ s.length + t.length : add_le_add (length_le_of_sublist (filter_sublist _)) (length_le_of_sublist (filter_sublist _))
+                                               ... < s.length + (t.length + 1) : add_lt_add_left (lt_add_one _) _ }
+end
+
+lemma sum_map_le_sum_map {l : list α} {f g : α → ℕ} (hfg : ∀ x ∈ l, f x ≤ g x) : (l.map f).sum ≤ (l.map g).sum :=
+begin
+  induction l with x l ih,
+  { refl },
+  { simp only [sum_cons, map],
+    exact add_le_add (hfg _ (mem_cons_self _ _)) (ih (λ x hx, hfg _ (mem_cons_of_mem _ hx))) }
+end
+
+lemma sum_map_lt_sum_map (l : list α) (f g : α → ℕ) (hfg : ∀ x ∈ l, f x ≤ g x) :
+  (l.map f).sum < (l.map g).sum ↔ ∃ x ∈ l, f x < g x :=
+begin
+  refine ⟨_, _⟩,
+  { induction l with a as ih,
+    { dec_trivial },
+    { by_cases h : f a < g a,
+      { exact λ _, ⟨a, ⟨mem_cons_self _ _, h⟩⟩ },
+      { have hfa : f a = g a := decidable.eq_iff_le_not_lt.2 ⟨hfg _ (mem_cons_self _ _), h⟩,
+        simp only [list.map_cons, list.sum_cons, hfa, add_lt_add_iff_left],
+        intro h',
+        obtain ⟨x, ⟨hx, hx'⟩⟩ := ih (λ x hx, hfg _ (mem_cons_of_mem _ hx)) h',
+        exact ⟨x, ⟨mem_cons_of_mem _ hx, hx'⟩⟩ } } },
+  { rintro ⟨x, ⟨hx, hx'⟩⟩,
+    obtain ⟨s, t, rfl⟩ := list.mem_split hx,
+    simp only [sum_cons, map, sum_append, map_append],
+    refine add_lt_add_of_le_of_lt (sum_map_le_sum_map (λ x hx, hfg _ (mem_append_left _ hx))) _,
+    exact add_lt_add_of_lt_of_le hx' (sum_map_le_sum_map (λ x hx, hfg _ (mem_append_right _ (mem_cons_of_mem _ hx)))) }
+end
+
+end list
+
 namespace literal
 
 end literal
@@ -13,16 +64,257 @@ namespace clause
 def unit_propagate (l : literal α) (c : clause α) : clause α :=
 c.filter $ (≠) l.inverse
 
+def unit_propagate' (l : literal α) (c : clause α) : option (clause α) :=
+if l ∈ c then none else some (unit_propagate l c)
+
+lemma unit_propagate'_of_mem {l : literal α} {c : clause α} (h : l ∈ c) : unit_propagate' l c = none :=
+by simp only [unit_propagate', h, if_true]
+
+lemma unit_propagate'_of_not_mem {l : literal α} {c : clause α} (h : l ∉ c) :
+  unit_propagate' l c = some (unit_propagate l c) :=
+by simp only [unit_propagate', h, if_false]
+
+lemma unit_propagate'_eq_some (l : literal α) (c d : clause α) :
+  unit_propagate' l c = some d ↔ l ∉ c ∧ unit_propagate l c = d :=
+by by_cases h : l ∈ c; simp [unit_propagate', h]
+
+@[simp]
+def satisfied' (ι : interpretation α) : option (clause α) → bool
+| none := tt
+| (some c) := satisfied ι c
+
+@[simp]
+def length' : option (clause α) → ℕ
+| none := 0
+| (some c) := c.length
+
+lemma length'_comp_some : (length' : option (clause α) → ℕ) ∘ some = list.length :=
+rfl
+
+lemma length_unit_propagate {l : literal α} {c : clause α} (hl : l.inverse ∈ c) : (unit_propagate l c).length < c.length :=
+begin
+  rw [unit_propagate, list.count_lt_iff_exists],
+  exact ⟨_, hl, λ h, h rfl⟩
+end
+
+lemma length'_unit_propagate'_of_mem {l : literal α} {c : clause α} (hl : l ∈ c) : length' (unit_propagate' l c) < c.length :=
+by simpa only [unit_propagate'_of_mem hl, length'] using list.length_pos_of_mem hl
+
+lemma length'_unit_propagate' {l : literal α} {c : clause α} (hl : l.inverse ∈ c) : length' (unit_propagate' l c) < c.length :=
+begin
+  by_cases h : l ∈ c,
+  { exact length'_unit_propagate'_of_mem h },
+  { rw [unit_propagate'_of_not_mem h, length'],
+    exact length_unit_propagate hl }
+end
+
+lemma length_unit_propagate_le (l : literal α) (c : clause α) : (unit_propagate l c).length ≤ c.length :=
+list.length_le_of_sublist $ list.filter_sublist c
+
+lemma length'_unit_propagate'_le (l : literal α) (c : clause α) : length' (unit_propagate' l c) ≤ c.length :=
+begin
+  by_cases h : l ∈ c,
+  { rw unit_propagate'_of_mem h,
+    exact zero_le _ },
+  { rw unit_propagate'_of_not_mem h,
+    exact length_unit_propagate_le l c }
+end
+
+lemma mem_unit_propagate {l : literal α} {c : clause α} {m : literal α} :
+  m ∈ unit_propagate l c ↔ m ∈ c ∧ l.inverse ≠ m :=
+by rw [unit_propagate, list.mem_filter]
+
+lemma satisfied_unit_propagate (l : literal α) (c : clause α) (ι : interpretation α) (hl : literal.satisfied ι l) :
+  satisfied ι (unit_propagate l c) ↔ satisfied ι c :=
+begin
+  simp only [satisfied_iff],
+  refine ⟨_, _⟩,
+  { rintro ⟨m, ⟨hmem, hm⟩⟩,
+    rw [mem_unit_propagate] at hmem,
+    exact ⟨m, ⟨hmem.1, hm⟩⟩ },
+  { rintro ⟨m, ⟨hmem, hm⟩⟩,
+    refine ⟨m, ⟨_, hm⟩⟩,
+    { rw mem_unit_propagate,
+      refine ⟨hmem, _⟩,
+      symmetry,
+      apply literal.not_satisfied_and_satisfied_inverse _ _ _ hm hl } }
+end
+
+lemma satisfied'_unit_propagate' (l : literal α) (c : clause α) (ι : interpretation α) (hl : literal.satisfied ι l) :
+  satisfied' ι (unit_propagate' l c) ↔ satisfied ι c :=
+begin
+  by_cases h : l ∈ c,
+  { rw [unit_propagate'_of_mem h, satisfied', satisfied_iff, coe_sort_tt, true_iff],
+    exact ⟨_, h, hl⟩ },
+  { rw [unit_propagate'_of_not_mem h, satisfied', satisfied_unit_propagate],
+    exact hl }
+end
+
 end clause
 
 namespace cnf
 
 def unit_propagate (l : literal α) (c : cnf α) : cnf α :=
-(c.filter $ (∉) l).map $ clause.unit_propagate l
+c.filter_map $ clause.unit_propagate' l
+
+lemma unit_propagate_cons (l : literal α) (γ : clause α) (c : cnf α) :
+  unit_propagate l (γ :: c) = if l ∈ γ then unit_propagate l c else (clause.unit_propagate l γ) :: unit_propagate l c :=
+begin
+  by_cases h : l ∈ γ,
+  { simp only [h, unit_propagate, clause.unit_propagate'_of_mem h, list.filter_map_cons_none, if_true] },
+  { simp only [h, unit_propagate, list.filter_map_cons_some _ _ _ (clause.unit_propagate'_of_not_mem h),
+      eq_self_iff_true, if_false, and_self] }
+end
+
+lemma sizeof_unit_propagate (l : literal α) (c : cnf α) :
+  sizeof (unit_propagate l c) = (c.map (clause.length' ∘ clause.unit_propagate' l)).sum :=
+begin
+  induction c with γ c ih,
+  { refl },
+  { simp only [sizeof_eq_size, size, unit_propagate_cons, list.sum_cons, function.comp_app, list.map],
+    by_cases h : l ∈ γ,
+    { simp only [h, clause.unit_propagate'_of_mem h, ←ih, sizeof_eq_size, size, if_true, clause.length', zero_add] },
+    { simp only [h, clause.unit_propagate'_of_not_mem h, ←ih, sizeof_eq_size, size, list.sum_cons, clause.length',
+      if_false, list.map] } }
+end
+
+lemma sizeof_cnf (c : cnf α) : sizeof c = (c.map (clause.length' ∘ some)).sum :=
+by rw [clause.length'_comp_some, sizeof_eq_size, size]
+
+lemma sizeof_unit_propagate_of_mem {l : literal α} {γ : clause α} {c : cnf α} (hlγ : l ∈ γ ∨ l.inverse ∈ γ) (hγc: γ ∈ c) :
+  sizeof (unit_propagate l c) < sizeof c :=
+begin
+  rw [sizeof_unit_propagate, sizeof_cnf, list.sum_map_lt_sum_map],
+  { cases hlγ,
+    { exact ⟨γ, hγc, clause.length'_unit_propagate'_of_mem hlγ⟩ },
+    { exact ⟨γ, hγc, clause.length'_unit_propagate' hlγ⟩ } },
+  { exact λ _ _, clause.length'_unit_propagate'_le _ _ }
+end
+
+lemma mem_unit_propagate (l : literal α) (c : cnf α) (γ : clause α) :
+  γ ∈ unit_propagate l c ↔ ∃ δ, (δ ∈ c ∧ l ∉ δ) ∧ clause.unit_propagate l δ = γ :=
+by simp only [unit_propagate, clause.unit_propagate'_eq_some, and.assoc, list.mem_filter_map]
+
+lemma non_mem_unit_propagate (l : literal α) (c : cnf α) (γ : clause α) (hγ : γ ∈ unit_propagate l c) : l ∉ γ :=
+begin
+  obtain ⟨δ, ⟨⟨hδc, hlδ⟩, rfl⟩⟩ := (mem_unit_propagate l c γ).1 hγ,
+  exact λ hl, hlδ (clause.mem_unit_propagate.1 hl).1
+end
+
+lemma inverse_non_mem_unit_propagate (l : literal α) (c : cnf α) (γ : clause α) (hγ : γ ∈ unit_propagate l c) :
+  l.inverse ∉ γ :=
+begin
+  obtain ⟨δ, ⟨⟨hδc, hlδ⟩, rfl⟩⟩ := (mem_unit_propagate l c γ).1 hγ,
+  exact λ hl, (clause.mem_unit_propagate.1 hl).2 rfl
+end
+
+lemma satisfied_unit_propagate (l : literal α) {c : cnf α} {ι : interpretation α} (hl : literal.satisfied ι l) :
+  satisfied ι (unit_propagate l c) ↔ satisfied ι c :=
+begin
+  simp only [satisfied_iff],
+  refine ⟨λ h γ hγ, _, λ h γ hγ, _⟩,
+  { by_cases hlγ : l ∈ γ,
+    { rw clause.satisfied_iff,
+      exact ⟨l, hlγ, hl⟩ },
+    { rw ←clause.satisfied_unit_propagate l _ _ hl,
+      apply h,
+      rw mem_unit_propagate,
+      exact ⟨γ, ⟨hγ, hlγ⟩, rfl⟩ } },
+  { rw mem_unit_propagate at hγ,
+    rcases hγ with ⟨δ, ⟨hδ, -⟩, rfl⟩, 
+    rw clause.satisfied_unit_propagate _ _ _ hl,
+    exact h _ hδ }
+end
+
+lemma satisfiable_of_satisfiable_unit_propagate {l : literal α} {c : cnf α} (h : satisfiable (unit_propagate l c)) :
+  satisfiable c :=
+begin
+  rcases h with ⟨ι, hι⟩,
+  by_cases h : literal.satisfied ι l = tt,
+  { refine ⟨ι, _⟩,
+    rw ←satisfied_unit_propagate _ h,
+    exact hι },
+  { refine ⟨function.update ι l.var (!ι l.var), _⟩,
+    rw ←satisfied_unit_propagate l _,
+    { rw satisfied_eq _ ι,
+      { exact hι },
+      { rintros γ hγ (m|m) hm,
+        { simp only [literal.satisfied],
+          suffices : m ≠ l.var,
+          { exact function.update_noteq this _ _ },
+          rintro rfl,
+          cases l,
+          { exact non_mem_unit_propagate _ _ _ hγ hm },
+          { exact inverse_non_mem_unit_propagate _ _ _ hγ hm } },
+        { simp only [literal.satisfied],
+          suffices : m ≠ l.var,
+          { congr' 1,
+            exact function.update_noteq this _ _ },
+          rintro rfl,
+          cases l,
+          { exact inverse_non_mem_unit_propagate _ _ _ hγ hm },
+          { exact non_mem_unit_propagate _ _ _ hγ hm } } } },
+    { cases l,
+      { simp only [eq_ff_eq_not_eq_tt, literal.var, literal.satisfied] at ⊢ h,
+        rw literal.var,
+        simpa only [eq_ff_eq_not_eq_tt, function.update_same, literal.bnot_iff_not] using h },
+      { simp only [bnot_eq_true_eq_eq_ff, eq_ff_eq_not_eq_tt, literal.var, eq_tt_eq_not_eq_ff, literal.satisfied,
+          literal.bnot_iff_not] at ⊢ h,
+        rw literal.var,
+        simpa only [bnot_eq_ff_eq_eq_tt, function.update_same] using h } } }
+end
+
+def any_literal : Π (c : clause α) (hc : c ≠ []), literal α
+| [] hc := false.elim (hc rfl)
+| (l::lc) hc := l
+
+lemma any_literal_mem : ∀ {c : clause α} (hc : c ≠ []), any_literal c hc ∈ c
+| [] hc := false.elim (hc rfl)
+| (l::lc) hc := list.mem_cons_self _ _
+
+#check has_well_founded_of_has_sizeof
 
 def dpll : cnf α → bool
 | [] := tt
-| ([]::xs) := ff
-| ((l::ls)::xs) := bor (unit_propagate l ((l::ls)::xs)).dpll (unit_propagate l ((l::ls)::xs)).dpll
+| (x::xs) := if h : [] ∈ x::xs then ff else
+    have hx : x ≠ [], from λ hx, h $ or.inl hx.symm,
+    have h1 : sizeof (unit_propagate (any_literal x hx) (x::xs)) < sizeof (x::xs),
+      from sizeof_unit_propagate_of_mem (or.inl (any_literal_mem hx)) (list.mem_cons_self _ _),
+    have h2 : sizeof (unit_propagate (any_literal x hx).inverse (x::xs)) < sizeof (x::xs), by
+    { refine sizeof_unit_propagate_of_mem (or.inr _) (list.mem_cons_self _ _),
+      simpa only [literal.inverse_inverse] using any_literal_mem hx },
+    dpll (unit_propagate (any_literal x hx) (x::xs)) || dpll (unit_propagate (any_literal x hx).inverse (x::xs))
+
+theorem dpll_correct : ∀ (c : cnf α), dpll c ↔ satisfiable c
+| [] := by simp only [dpll, satisfiable_empty, coe_sort_tt]
+| (x::xs) := if h : [] ∈ x::xs then by simpa [dpll, h] using not_satisfiable_of_empty_mem h else
+    have hx : x ≠ [], from λ hx, h $ or.inl hx.symm,
+    have h1 : sizeof (unit_propagate (any_literal x hx) (x::xs)) < sizeof (x::xs),
+      from sizeof_unit_propagate_of_mem (or.inl (any_literal_mem hx)) (list.mem_cons_self _ _),
+    have h2 : sizeof (unit_propagate (any_literal x hx).inverse (x::xs)) < sizeof (x::xs), by
+    { refine sizeof_unit_propagate_of_mem (or.inr _) (list.mem_cons_self _ _),
+      simpa only [literal.inverse_inverse] using any_literal_mem hx },
+    begin
+      simp only [dpll, h, dpll_correct, bor_coe_iff, bor, dif_neg, not_false_iff, literal.inverse],
+      clear h1 h2,
+      refine ⟨_, _⟩,
+      { rintro (hr|hr),
+        { apply satisfiable_of_satisfiable_unit_propagate hr },
+        { apply satisfiable_of_satisfiable_unit_propagate hr } },
+      { rintro ⟨ι, hι⟩,
+        by_cases hlι : literal.satisfied ι (any_literal x hx) = tt,
+        { exact or.inl ⟨ι, (satisfied_unit_propagate _ hlι).2 hι⟩ },
+        { refine or.inr ⟨ι, (satisfied_unit_propagate _ _).2 hι⟩,
+          simpa only [literal.satisfied_inverse, literal.bnot_iff_not] using hlι } }
+    end
+
+#print axioms dpll_correct
+
+section
+open literal
+
+#eval dpll [[pos 5], [neg 5, pos 6], [neg 6]]
+
+end
 
 end cnf
